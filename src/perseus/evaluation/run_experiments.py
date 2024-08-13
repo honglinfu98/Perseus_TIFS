84cd9fc039280e89de6c74f6ff8c9ab2e8edda3c
honglinfu98 <honglin.fu.22@ucl.ac.uk>
2024-08-13 23:37:32 +0800

    multi threads

--- src/perseus/evaluation/run_experiments.py
+++ src/perseus/evaluation/run_experiments.py
@@ -8,6 +8,7 @@ import random
 import pickle
 import torch
 import numpy as np
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from sklearn.metrics import (
     roc_curve,
     precision_score,
@@ -87,7 +88,6 @@ def run_experiment(model, train_loader, test_loader, num_epochs=100):
         return all_probs, all_labels, all_embs, batch_times, num_nodes
 
     train()
-    print(len(test(test_loader)))  # Check the number of returned values
 
     probs, labels, embs, batch_times, num_nodes = test(test_loader)
     probs = torch.cat(probs, dim=0).sigmoid().numpy()
@@ -197,93 +197,127 @@ dataset_colors = {"DDINA": "blue", "COSS": "green", "DDM": "red"}
 # Non time dimension related experiments
 results = {"DDINA": {}, "COSS": {}, "DDM": {}}
 
-for dataset in datasets:
-    train_loader, test_loader, _ = get_split_data_pickle(dataset)
-    for model_name in models:
-        if model_name == "GAT":
-            if dataset == "DDINA":
-                num_features = 4
-            else:
-                num_features = 2
-            model = Net(num_features, num_classes)
-        elif model_name == "GCN":
-            if dataset == "DDINA":
-                num_features = 4
-            else:
-                num_features = 2
-            model = GCNNet(num_features, hidden_channels, num_classes)
-        elif model_name == "GraphSAGE":
-            if dataset == "DDINA":
-                num_features = 4
-            else:
-                num_features = 2
-            model = GraphSAGENet(num_features, hidden_channels, num_classes)
+# for dataset in datasets:
+#     if dataset == "DDINA":
+#         num_features = 4
+#     else:
+#         num_features = 2
+#     train_loader, test_loader, _ = get_split_data_pickle(dataset)
+#     for model_name in models:
+#         if model_name == "GAT":
+#             model = Net(num_features, num_classes)
+#         elif model_name == "GCN":
+#             model = GCNNet(num_features, hidden_channels, num_classes)
+#         elif model_name == "GraphSAGE":
+#             model = GraphSAGENet(num_features, hidden_channels, num_classes)
 
-        print(f"Running {model_name} Experiment on {dataset}")
-        m, fpr, tpr, _, train_times, batch_times, num_nodes, embs, labels = (
-            run_experiment(model, train_loader, test_loader, num_epochs=100)
-        )
-        metrics = compute_metrics(model, test_loader)
-        # Store results along with training times
-        results[dataset][model_name] = {
-            "model": m,
-            "metrics": metrics,
-            "fpr": fpr,
-            "tpr": tpr,
-            "train_times": train_times,  # Storing the training times
-            "batch_times": batch_times,
-            "num_nodes": num_nodes,
-            "embs": embs,
-            "labels": labels,
-        }
+#         print(f"Running {model_name} Experiment on {dataset}")
+#         m, fpr, tpr, _, train_times, batch_times, num_nodes, embs, labels = (
+#             run_experiment(model, train_loader, test_loader, num_epochs=100)
+#         )
+#         metrics = compute_metrics(model, test_loader)
+#         # Store results along with training times
+#         results[dataset][model_name] = {
+#             "model": m,
+#             "metrics": metrics,
+#             "fpr": fpr,
+#             "tpr": tpr,
+#             "train_times": train_times,  # Storing the training times
+#             "batch_times": batch_times,
+#             "num_nodes": num_nodes,
+#             "embs": embs,
+#             "labels": labels,
+#         }
 
 
-results1 = {"DDINA": {}, "COSS": {}, "DDM": {}}
+def experiment_pipeline(model_name, dataset, train_loader, test_loader, num_epochs=100):
+    if dataset == "DDINA":
+        num_features = 4
+    else:
+        num_features = 2
+
+    if model_name == "GAT":
+        model = Net(num_features, num_classes)
+    elif model_name == "GCN":
+        model = GCNNet(num_features, hidden_channels, num_classes)
+    elif model_name == "GraphSAGE":
+        model = GraphSAGENet(num_features, hidden_channels, num_classes)
+
+    print(f"Running {model_name} Experiment on {dataset}")
+    m, fpr, tpr, _, train_times, batch_times, num_nodes, embs, labels = run_experiment(
+        model, train_loader, test_loader, num_epochs=num_epochs
+    )
+    metrics = compute_metrics(model, test_loader)
+    return {
+        "model": m,
+        "metrics": metrics,
+        "fpr": fpr,
+        "tpr": tpr,
+        "train_times": train_times,
+        "batch_times": batch_times,
+        "num_nodes": num_nodes,
+        "embs": embs,
+        "labels": labels,
+    }
 
 
-for dataset in datasets:
-    train_loader, test_loader, _ = get_train_test_validate_data_pickle(dataset)
-    for model_name in models:
-        if model_name == "GAT":
-            if dataset == "DDINA":
-                num_features = 4
-            else:
-                num_features = 2
-            model = Net(num_features, num_classes)
-        elif model_name == "GCN":
-            if dataset == "DDINA":
-                num_features = 4
-            else:
-                num_features = 2
-            model = GCNNet(num_features, hidden_channels, num_classes)
-        elif model_name == "GraphSAGE":
-            if dataset == "DDINA":
-                num_features = 4
-            else:
-                num_features = 2
-            model = GraphSAGENet(num_features, hidden_channels, num_classes)
+with ThreadPoolExecutor(max_workers=12) as executor:
+    future_to_model = {}
+    for dataset in datasets:
+        train_loader, test_loader, _ = get_split_data_pickle(dataset)
+        for model_name in models:
+            future = executor.submit(
+                experiment_pipeline, model_name, dataset, train_loader, test_loader
+            )
+            future_to_model[future] = (dataset, model_name)
 
-        print(f"Running {model_name} Experiment on {dataset}")
-        m, fpr, tpr, _, train_times, batch_times, num_nodes, embs, labels = (
-            run_experiment(model, train_loader, test_loader, num_epochs=100)
-        )
-        metrics = compute_metrics(model, test_loader)
-        # Store results along with training times
-        results1[dataset][model_name] = {
-            "model": m,
-            "metrics": metrics,
-            "fpr": fpr,
-            "tpr": tpr,
-            "train_times": train_times,  # Storing the training times
-            "batch_times": batch_times,
-            "num_nodes": num_nodes,
-            "embs": embs,
-            "labels": labels,
-        }
+    for future in as_completed(future_to_model):
+        dataset, model_name = future_to_model[future]
+        try:
+            result = future.result()
+            results[dataset][model_name] = result
+            print(f"Completed {model_name} Experiment on {dataset}")
+        except Exception as exc:
+            print(f"{model_name} experiment on {dataset} generated an exception: {exc}")
+
+# results1 = {"DDINA": {}, "COSS": {}, "DDM": {}}
+
+
+# for dataset in datasets:
+#     if dataset == "DDINA":
+#         num_features = 4
+#     else:
+#         num_features = 2
+#     train_loader, test_loader, _ = get_train_test_validate_data_pickle(dataset)
+#     for model_name in models:
+#         if model_name == "GAT":
+#             model = Net(num_features, num_classes)
+#         elif model_name == "GCN":
+#             model = GCNNet(num_features, hidden_channels, num_classes)
+#         elif model_name == "GraphSAGE":
+#             model = GraphSAGENet(num_features, hidden_channels, num_classes)
+
+#         print(f"Running {model_name} Experiment on {dataset}")
+#         m, fpr, tpr, _, train_times, batch_times, num_nodes, embs, labels = (
+#             run_experiment(model, train_loader, test_loader, num_epochs=100)
+#         )
+#         metrics = compute_metrics(model, test_loader)
+#         # Store results along with training times
+#         results1[dataset][model_name] = {
+#             "model": m,
+#             "metrics": metrics,
+#             "fpr": fpr,
+#             "tpr": tpr,
+#             "train_times": train_times,  # Storing the training times
+#             "batch_times": batch_times,
+#             "num_nodes": num_nodes,
+#             "embs": embs,
+#             "labels": labels,
+#         }
 
 with open(path.join(PROJECT_ROOT, "data", "results.pkl"), "wb") as file:
     pickle.dump(results, file)
 
 
-with open(path.join(PROJECT_ROOT, "data", "results1.pkl"), "wb") as file:
-    pickle.dump(results1, file)
+# with open(path.join(PROJECT_ROOT, "data", "results1.pkl"), "wb") as file:
+#     pickle.dump(results1, file)
